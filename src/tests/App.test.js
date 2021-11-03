c1b7ded5d4b7c41291426912add905793d45df06
viniciusantana <viniciusantana@yahoo.com.br>
2021-11-03 19:31:07 -0300

    Feat: Todos os requisitos concluidos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,42 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import responseAPI from './mocks';
 
 describe('Test Rick & Morty API', () => {
 
   beforeEach(()=>{
     //Fazer o mock do fetch aqui 
-    
+    global.fetch = jest.fn(async () => ({
+      json: async () => responseAPI
+    }));
     render(<App/>)
   })
   
-  test('Verifica se aparece o card com titulo de "Rick Sanchez"', () => {
+  test('Verifica se aparece o card com titulo de "Rick Sanchez"', async () => {
+    const rick = await screen.findByRole('heading', {
+      level: 3,
+      name: 'Rick Sanchez',
+    });
+    expect(rick).toBeInTheDocument();
 
   })
 
   test('Verifica se existem o input de texto e o botão "Buscar"', () => {
-    
+    const input = screen.getByRole('textbox');
+    const button = screen.getByRole('button', {name: 'Buscar',});
+
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+
+
   })
 
-  test('Verifica se ao buscar por "Smith" aparecem 4 cards', () => {
-    
+  test('Verifica se ao buscar por "Smith" aparecem 4 cards', async () => {
+    const smith = await screen.findAllByRole('heading', {
+      level: 3,
+      name: /smith/i,
+    });
+    expect(smith).toHaveLength(4);
   })
 
 })
